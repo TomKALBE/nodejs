9128aa9a6768d1786ca32d15812c3b05886794d4
TomKALBE <t.kalbe@h3hitema.fr>
2022-04-29 09:43:11 +0200

    Lecture de fichier depuis un autre dossier qui fonctionne

--- exo4/src/index.js
+++ exo4/src/index.js
@@ -1,13 +1,6 @@
 fs = require('fs');
 const path = require('path');
 
-fs.readdir(
-    path.resolve(__dirname, 'index.js'),
-    (err, files) => {
-      if (err) throw err;
-      
-      for (let file of files) {
-        console.log(file);
-      }
-    }
-  );
+console.log(fs.readFileSync(`${__dirname}/log.txt`, "utf-8"));
+
+console.log(fs.readFileSync(`${__dirname}/../assets/index.html`, "utf-8"));
