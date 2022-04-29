9ee4ee68c3f47ae76df0fdba5967ff1b76a045fb
TomKALBE <t.kalbe@h3hitema.fr>
2022-04-29 09:39:22 +0200

    Lecture de fichier à partir d'un autre dossier

--- exo4/src/index.js
+++ exo4/src/index.js
@@ -1,3 +1,13 @@
 fs = require('fs');
 const path = require('path');
 
+fs.readdir(
+    path.resolve(__dirname, 'index.js'),
+    (err, files) => {
+      if (err) throw err;
+      
+      for (let file of files) {
+        console.log(file);
+      }
+    }
+  );
